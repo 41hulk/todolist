9b95ed41877ea7473920a6d884b5a65aea4f2984
Ntare Guy <gntare2@gmail.com>
2022-02-22 15:50:14 +0200

    functionality

--- src/modules/complete.js
+++ src/modules/complete.js
@@ -1,10 +1,10 @@
 export default class Completed {
-  static updateLocalStorage = (task) => {
+  static updateLocalStorage(task) {
     const data = JSON.stringify(task);
     localStorage.setItem('todo', data);
   }
 
-  static completeTask = (task) => {
+  static completeTask(task) {
     this.change = document.querySelectorAll('.checkbox');
     this.change.forEach((element, index) => {
       element.addEventListener('change', () => {
@@ -12,10 +12,14 @@ export default class Completed {
           element.nextElementSibling.classList.toggle('check');
           element.checked = true;
           task[index].completed = true;
+          element.nextElementSibling.nextElementSibling.classList.add('fa-trash-o');
+          element.nextElementSibling.nextElementSibling.classList.remove('fa-ellipsis-v');
         } else {
           element.nextElementSibling.classList.toggle('check');
           element.checked = false;
           task[index].completed = false;
+          element.nextElementSibling.nextElementSibling.classList.remove('fa-trash-o');
+          element.nextElementSibling.nextElementSibling.classList.add('fa-ellipsis-v');
         }
         Completed.updateLocalStorage(task);
       });
